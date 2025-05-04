2271796a4321223cdabf95fd79297cb834cced3c
Fred Dante <197927269+Ogunnubi@users.noreply.github.com>
2025-05-04 13:14:24 +0100

    Create job.py

---
+++ job-cred-system-backend-main/app/schemas/job.py
@@ -0,0 +1,32 @@
+# schemas/job.py
+from pydantic import BaseModel
+from typing import Optional
+
+class JobIn(BaseModel):
+    title: str
+    job_description: str
+    credits_required: int
+    posted_by: Optional[str] = None
+
+
+class JobOut(BaseModel):
+    id: str
+    title: str
+    job_description: str
+    credits_required: int
+    posted_by: Optional[str] = None
+    created_at: Optional[str]
+
+    class Config:
+        from_attributes = True
+
+class JobApplicationIn(BaseModel):
+    proposal: str
+
+class JobApplicationOut(BaseModel):
+    id: str
+    job_id: str
+    user_id: str
+    proposal: str
+    status: str
+    created_at: str
